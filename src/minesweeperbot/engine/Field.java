6a173830e952be925df04a3dc57afa824c801066
Thomas MONTERO <thomas99.montero@gmail.com>
2019-02-15 16:53:25 +0100

    field claas for the field of the minesweeper done

--- src/minesweeperbot/engine/Field.java
+++ src/minesweeperbot/engine/Field.java
@@ -1,5 +1,35 @@
 package minesweeperbot.engine;
 
 public class Field {
+	private Cell[][] field;
+	private int width;
+	private int height;
 
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getHeight() {
+		return this.height;
+	}
+
+	public Cell getCell(int x, int y) {
+		return this.field[x][y];
+	}
+
+	public Field(int width, int height) {
+		if (width <=0 || height <=0) throw new IllegalArgumentException("tables must have a size");
+
+		this.field = new Cell[width][height];
+
+		//adding cells
+		for (int i = 0; i< width; i++) {
+			for (int j = 0; j< height; j++) {
+				this.field[i][j] = new Cell();
+			}
+		}
+
+		this.width = width;
+		this.height = height;
+	}
 }
