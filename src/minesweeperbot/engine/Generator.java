b3a1d55c95bb50370a47e89407b86bd3ead4c9c7
Thomas MONTERO <thomas99.montero@gmail.com>
2019-02-22 14:44:05 +0100

    using now less getters and now using variables

--- src/minesweeperbot/engine/Generator.java
+++ src/minesweeperbot/engine/Generator.java
@@ -11,7 +11,9 @@ public class Generator {
 	}
 
 	public static void generate(Field playground, int mines) {
-		int size = playground.getHeight()*playground.getWidth();
+		int height = playground.getHeight();
+		int width = playground.getWidth();
+		int size = height*width;
 		if (mines>=size) throw new IllegalArgumentException("Too much mines !");
 		List<Boolean> shuffled = new ArrayList<Boolean>(size);
 
@@ -29,25 +31,25 @@ public class Generator {
 		//porting the shuffling to the playground
 		for (int i=0; i<size; i++) {
 			if (shuffled.get(i)) {
-				playground.getCell(i%playground.getWidth(), i/playground.getWidth()).setMine(true);
+				playground.getCell(i%width, i/width).setMine(true);
 			} else {
-				playground.getCell(i%playground.getWidth(), i/playground.getWidth()).setMine(false);
+				playground.getCell(i%width, i/width).setMine(false);
 			}
 		}
 
 		//setting the proximity
-		for (int x=0; x<playground.getWidth(); x++) {
-			for (int y=0; y<playground.getHeight(); y++) {
+		for (int x=0; x<width; x++) {
+			for (int y=0; y<height; y++) {
 				if (!playground.getCell(x, y).isMine()) {
 					int proximity = 0;
 					if (x-1>=0 && y-1>=0 && playground.getCell(x-1,y-1).isMine()) proximity++;
 					if (x-1>=0 && playground.getCell(x-1,y).isMine()) proximity++;
-					if (x-1>=0 && y+1<playground.getHeight() && playground.getCell(x-1,y+1).isMine()) proximity++;
+					if (x-1>=0 && y+1<height && playground.getCell(x-1,y+1).isMine()) proximity++;
 					if (y-1>=0 && playground.getCell(x,y+1).isMine()) proximity++;
-					if (y+1<playground.getHeight() && playground.getCell(x,y+1).isMine()) proximity++;
-					if (x+1<playground.getWidth() && y-1>=0 && playground.getCell(x+1,y-1).isMine()) proximity++;
-					if (x+1<playground.getWidth() && playground.getCell(x+1,y).isMine()) proximity++;
-					if (x+1<playground.getWidth() && y+1<playground.getHeight() && playground.getCell(x+1,y+1).isMine()) proximity++;
+					if (y+1<height && playground.getCell(x,y+1).isMine()) proximity++;
+					if (x+1<width && y-1>=0 && playground.getCell(x+1,y-1).isMine()) proximity++;
+					if (x+1<width && playground.getCell(x+1,y).isMine()) proximity++;
+					if (x+1<width && y+1<height && playground.getCell(x+1,y+1).isMine()) proximity++;
 
 					playground.getCell(x, y).setProximity(proximity);
 				}
