f88e092476153f9de619b4402811653f6ad7baeb
Thomas MONTERO <thomas99.montero@gmail.com>
2019-02-25 21:36:31 +0100

    added reveal and toggle flagmethodes

--- src/minesweeperbot/engine/Engine.java
+++ src/minesweeperbot/engine/Engine.java
@@ -1,10 +1,14 @@
 package minesweeperbot.engine;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 public class Engine {
 	private Field playground;
+	private GameState gameState;
 	private int mines;
 	private int width;
 	private int height;
+	private int counter;
 
 	public Engine(int mines, int width, int height) {
 		this.mines = mines;
@@ -19,7 +23,41 @@ public class Engine {
 	}
 
 	public void newGame() {
+		counter = 0;
+		gameState = GameState.PLAYING;
 		this.playground = new Field(width,height);
 		Generator.generate(this.playground,mines);
+	}
+
+	private void revealImp(int x, int y) {
+		if (x<0 || x>=width || y<0 || y>=height) return;
+		playground.getCell(x,y).reveal();
+		if (playground.getCell(x,y).isMine()) {
+			gameState = GameState.LOSS;
+		}
+
+		if (playground.getCell(x,y).getProximity()==0) {
+			revealImp(x-1,y-1);
+			revealImp(x-1,y);
+			revealImp(x-1,y+1);
+			revealImp(x,y+1);
+			revealImp(x,y+1);
+			revealImp(x+1,y-1);
+			revealImp(x+1,y);
+			revealImp(x+1,y+1);
+		}
+	}
+
+	public void reveal(int x, int y) {
+		if (counter==0) {
+			while (playground.getCell(x,y).isMine()) {
+				newGame();
+			}
+		}
+		revealImp(x,y);
+	}
+
+	public void toggleFlag (int x, int y) {
+		playground.getCell(x,y).toggleFlagged();
 	}
 }
